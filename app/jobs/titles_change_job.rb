08028e2ed2ce602dd2985a27c670856decdfe771
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-18 11:48:25 +0900

    add: active job実装

---
+++ app/jobs/titles_change_job.rb
@@ -0,0 +1,6 @@
+class TitlesChangeJob < ApplicationJob
+  queue_as :default
+  def perform(blog_id)
+    Blog.find(blog_id).title_change
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Blogappp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    config.active_job.queue_adapter = :delayed_job
+  end
+end
